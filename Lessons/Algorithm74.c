9d5a9a294f55e364a853a869e862475e5d30e12a
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-05-02 09:03:40 +0300

    Fixes of commit 18

--- Lessons/Algorithm74.c
+++ Lessons/Algorithm74.c
@@ -4,14 +4,30 @@
 int main(){
     char str[101];
     char *p;
+    int i = 0, j = 0;
 
     printf("Enter name: ");
     fgets(str, 100, stdin);
     if(p = strchr(str, '\n')) *p = '\0'; 
 
-    do {
-        if(p = strchr(str, ' ')) *p = '\n';
-    } while (p);
+    while (str[i] == ' ') {
+        i++;
+    }
 
+    for (; str[i] != '\0'; i++) {
+        if (str[i] != ' ' || str[i + 1] != ' ') {
+            str[j++] = str[i];
+        }
+    }
+
+    if (j > 0 && str[j - 1] == ' ') {
+        j--;
+    }
+    str[j] = '\0';
+    do{
+        if(p=strchr(str, ' ')) {
+            *p = '\n';
+        }
+    }while (p);
     printf("Your name is:\n%s\n", str);
 }
