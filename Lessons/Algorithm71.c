4f6df2f37e1556a7707e961b47c2e4ecd280cfda
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-05-06 18:42:14 +0300

    18mini fixes

--- Lessons/Algorithm71.c
+++ Lessons/Algorithm71.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-
+    //search and replace
 void main()
 {
     char s[100],s1[100],s2[100];
@@ -8,8 +8,13 @@ void main()
     int br=0;
     printf("s=");fgets(s,100,stdin);
     if(p=strchr(s,'\n'))*p='\0';
+
     
-    //search and replace
+    printf("s1=");fgets(s1,100,stdin);
+    if(p=strchr(s1,'\n'))*p='\0';
+    printf("s2=");fgets(s2,100,stdin);
+    if(p=strchr(s2,'\n'))*p='\0';
+
     char spom[100];
     int len1=strlen(s1);
     int len2=strlen(s2);
