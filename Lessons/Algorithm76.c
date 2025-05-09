b0ca3c2a90f2e42c468687c3e98fc4679d035fa9
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-05-09 08:50:13 +0300

    Commit 19

---
+++ Lessons/Algorithm76.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#define max_students 30
+#define max_subjects 20
+
+void main()
+{
+    int n, m, i, j;
+    float avr = 0;
+    int marks[max_students][max_subjects];
+    
+    do {
+        printf("Enter the number of students: ");
+        scanf("%d", &n);
+    } while (n <= 0 || n > max_students);
+
+    do {
+        printf("Enter the number of subjects: ");
+        scanf("%d", &m);
+    } while (m <= 0 || m > max_subjects);
+
+    for(i=0; i < n; i++) {
+        printf("Enter marks for student %d:\n", i + 1);
+        for(j = 0; j < m; j++) {
+            do{
+                printf("Enter marks for subject %d: ", j + 1);
+                scanf("%d", &marks[i][j]);
+            }while(marks[i][j] < 2 || marks[i][j] > 6);
+        }
+    }
+    printf("\nMarks entered:\n");
+    printf("Subjects:   ");
+    for(j = 0; j < m; j++) {
+        printf("%d    ", j + 1);
+    }
+    printf("Average: ");
+    printf("\n");
+    for(i = 0; i < n; i++) {
+        printf("Student %d:  ", i + 1);
+        for(j = 0; j < m; j++) {
+            avr += marks[i][j];
+            printf("%d    ", marks[i][j]);
+        }
+        printf("%.2f", avr / m);
+        avr = 0;
+        printf("\n");
+    }
+    printf("Average:    ");
+    for(j = 0; j < m; j++) {
+        avr = 0;
+        for(i = 0; i < n; i++) {
+            avr += marks[i][j];
+        }
+        printf("%.2f ", avr / n);
+    }
+
+    avr = 0;
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            avr += marks[i][j];
+        }
+    }
+    printf("%.2f", avr / (n * m));
+
+}
---
+++ Lessons/Algorithm77.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 251
+
+int main() {
+    char c[MAX];
+    int a[125][2];
+    char *p;
+    int j, i = 0, open = 0, closed = 0, br = 0;
+
+    printf("Enter string: ");
+    fgets(c, MAX, stdin);
+    if ((p = strchr(c, '\n'))) *p = '\0';
+
+    if (strchr(c, '(') && strchr(c, ')')) {
+        for (i = 0; c[i]; i++) {
+            if(c[i] != '(' && c[i] != ')' && c[i] != '\0') {
+                printf("Invalid brackets\n");
+                return 0;
+            }
+        }
+
+        while (c[i]) {
+            if (c[i] == '(') br++;
+            else if (c[i] == ')') br--;
+            if (br < 0) {
+                printf("Invalid brackets\n");
+                return 0;
+            }
+            i++;
+        }
+
+        if (br != 0) {
+            printf("Invalid brackets\n");
+            return 0;
+        }
+
+        for (i = 0; c[i]; i++) {
+            if (c[i] == '(') open++;
+            else if (c[i] == ')') closed++;
+        }
+    
+
+        if (open != closed) {
+            printf("Invalid brackets\n");
+            return 0;
+        }
+        br = 0;
+        for (i = 0; c[i]; i++) {
+            if (c[i] == ')') {
+                j = i - 1;
+                while (j >= 0 && c[j] != '(') {
+                    j--;
+                }
+                if (j >= 0) {
+                    a[br][0] = j + 1;
+                    a[br][1] = i + 1;
+                    br++;
+                    c[j] = ' ';
+                }
+            }
+        }
+        int temp[2];
+        for (i = 0; i < br - 1; i++) {
+            for (j = 0; j < br - 1 - i; j++) {
+                if (a[j][0] > a[j + 1][0]) {
+                    temp[0] = a[j][0];
+                    temp[1] = a[j][1];
+                    a[j][0] = a[j + 1][0];
+                    a[j][1] = a[j + 1][1];
+                    a[j + 1][0] = temp[0];
+                    a[j + 1][1] = temp[1];
+                }
+            }
+        }
+        for (i = 0; i < br; i++) {
+            printf("(%d, %d)\n", a[i][0], a[i][1]);
+        }
+    } else {
+        printf("Invalid brackets\n");
+        return 0;
+    }
+
+    return 0;
+}
