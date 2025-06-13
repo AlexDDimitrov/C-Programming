3ea5eda21624414eb854b99eb24cbdc236f2431c
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-06-13 13:02:00 +0300

    algorythm 81 finished

--- Lessons/Algorithm81/Algorithm81.c
+++ Lessons/Algorithm81/Algorithm81.c
@@ -6,9 +6,10 @@
 int main()
 {
     int a = 1; 
-    int n, m, i, j;
+    int m;
     char c;
     int nfile;
+    char *p;
     char filename[20];
     char line[100];
     m=0;
@@ -51,7 +52,8 @@ int main()
             case 1:
                 system("cls");
                 printf("Enter filename: ");
-                scanf("%s", filename);
+                fgets(filename, sizeof(filename), stdin);
+                if ((p = strchr(filename, '\n'))) *p = '\0';
                 printf("File '%s' selected.\n", filename);
                 break;
             case 2:
@@ -67,38 +69,57 @@ int main()
                 } else {
                     for (int ifile = 0; ifile < nfile; ifile++) {
                         fgets(line, sizeof(line), stdin);
-                        fprintf(f, "%s\n", line);
-                        if (strstr(line, "and\n") != NULL) {
+                        if (strcmp(line, "and\n") == 0) {
                             char *p;
                             if ((p = strchr(line, 'and\n'))) *p = '\n';
                             printf("Ending input...\n");
                             break;
                         }
-                        fprintf(f, "%s\n", line);
+                        fprintf(f, "%s", line);
                     }
                     fclose(f);
-                    printf("Written %d lines to '%s'.\n", nfile, filename);
                 }
                 break;
             case 3:
                 if (strlen(filename) == 0) {
                 printf("No file selected! Choose a file first.\n");
                 break;
-            }
-
-            FILE *fread = fopen(filename, "r");
-            if (!fread) {
-                printf("Error opening file '%s' for reading.\n", filename);
-            } else {
-                printf("Contents of '%s':\n\n", filename);
-                while (fgets(line, sizeof(line), fread)) {
-                    printf("%s", line);
                 }
-                fclose(fread);
-            }
-            break;
+
+                FILE *fread = fopen(filename, "r");
+                if (!fread) {
+                    printf("Error opening file '%s' for reading.\n", filename);
+                } else {
+                    printf("Contents of '%s':\n", filename);
+                    while (fgets(line, sizeof(line), fread)) {
+                        printf("%s", line);
+                    }
+                    fclose(fread);
+                }
+                break;
             case 4:
-                //need to append to the file
+                if (strlen(filename) == 0) {
+                    printf("No file selected! Choose a file first.\n");
+                    break;
+                }
+                system("cls");
+                nfile = 100;
+                FILE *fappend = fopen(filename, "a");
+                if (!fappend) {
+                    printf("Error opening file.\n");
+                } else {
+                    for (int ifile = 0; ifile < nfile; ifile++) {
+                        fgets(line, sizeof(line), stdin);
+                        if (strcmp(line, "and\n") == 0) {
+                            char *p;
+                            if ((p = strchr(line, 'and\n'))) *p = '\n';
+                            printf("Ending input...\n");
+                            break;
+                        }
+                        fprintf(fappend, "%s", line);
+                    }
+                    fclose(fappend);
+                }
                 break;
             case 5:
                 printf("Quitting...\n");
