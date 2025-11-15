f8116e31699f6aae63b28087697cb068c523b9c4
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-11-15 11:08:05 +0200

    12.11DONE

---
+++ Lessons 25-26/12.11/mainFactoriel.c
@@ -0,0 +1,9 @@
+#include "maths.h"
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int result = factoriel(a);
+    printf("Factorial of %d is %d\n", a, result);
+    return 0;
+}
---
+++ Lessons 25-26/12.11/maths.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int factoriel(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
