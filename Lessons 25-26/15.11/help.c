ed94fa6f63f6db5dc530421eb34267ce1b3cdb39
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-11-16 17:07:23 +0200

    Typo error

--- Lessons 25-26/15.11/help.c
+++ Lessons 25-26/15.11/help.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("This is the help program. Use the following commands:\n");
-    printf("1. strlenght.exe <string> - to get the length of a string.\n");
+    printf("1. strlength.exe <string> - to get the length of a string.\n");
     printf("2. strconcat.exe <string1> <string2> - to concatenate two strings.\n");
     printf("3. strcompare.exe <string1> <string2> - to compare two strings.\n");
     return 0;
