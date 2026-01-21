f51b45fd72380309b14abd140b7fd805228e4de7
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-01-21 14:01:33 +0200

    упдате

--- My projects/Minesweeper/minesweeper.c
+++ My projects/Minesweeper/minesweeper.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Time.h>
+#include <time.h>
 #include <ctype.h>
+#include <string.h>
+
+#define COLOR_BROWN "\033[33m"
+#define COLOR_WHITE "\033[97m"
+#define COLOR_RESET "\033[0m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_LIGHT_BLUE "\033[94m"
+#define COLOR_LIGHT_GREEN "\033[92m"
+#define COLOR_DARK_BLUE "\033[34m"
+#define COLOR_GRAY "\033[90m"
+#define COLOR_DARK_RED "\033[91m"
+#define COLOR_DARK_GRAY "\033[90m"
+#define COLOR_DARK_GREEN "\033[32m"
 
 char board[16][16];
 char memory[16][16];
@@ -9,21 +24,43 @@ char memory[16][16];
 void initialize_board() {
     for (int i = 0; i < 16; i++) {
         for (int j = 0; j < 16; j++) {
-            board[i][j] = '0';
+            board[i][j] = '?';
+            memory[i][j] = ' ';
         }
     }
 }
 
 void print_board() {
     for (int i = 15; i >= 0; i--) {
-        if (i + 1 < 10)
-            printf(" %d  ", i + 1);
-        else {
-            printf("%d  ", i + 1);
-        }
+        printf("%2d  ", i + 1);
         for (int j = 0; j < 16; j++) {
-            char piece = memory[i][j];
-            printf("%c ", piece);
+            char display_char = memory[i][j];
+            if (display_char == ' ') {
+                if ((i + j) % 2 == 0)
+                    printf(COLOR_GREEN "- " COLOR_RESET);
+                else
+                    printf(COLOR_DARK_GREEN "- " COLOR_RESET);
+            } else if (board[i][j] == '*') {
+                printf(COLOR_RED "%c " COLOR_RESET, display_char);
+            } else if (display_char == '0') {
+                printf(COLOR_WHITE "%c " COLOR_RESET, display_char);
+            } else if (display_char == '1') {
+                printf(COLOR_BLUE "%c " COLOR_RESET, display_char);
+            } else if (display_char == '2') {
+                printf(COLOR_LIGHT_GREEN "%c " COLOR_RESET, display_char);
+            } else if (display_char == '3') {
+                printf(COLOR_RED "%c " COLOR_RESET, display_char);
+            } else if (display_char == '4') {
+                printf(COLOR_DARK_BLUE "%c " COLOR_RESET, display_char);
+            } else if (display_char == '5') {
+                printf(COLOR_DARK_RED "%c " COLOR_RESET, display_char);
+            } else if (display_char == '6') {
+                printf(COLOR_LIGHT_BLUE "%c " COLOR_RESET, display_char);
+            } else if (display_char == '7') {
+                printf(COLOR_DARK_GRAY "%c " COLOR_RESET, display_char);
+            } else if (display_char == '8') {
+                printf(COLOR_GRAY "%c " COLOR_RESET, display_char);
+            }
         }
         printf("\n");
     }
@@ -31,92 +68,80 @@ void print_board() {
 }
 
 void create_mine(int row, int col) {
-    srand(time(NULL));
-    for (int x = 0; x < 16; x++)
+    for (int x = 0; x < 16; x++) {
         for (int y = 0; y < 16; y++) {
-            int r = rand() % 100;
             if (x == row && y == col) {
-                board[x][y] = ' '; // Ensure the selected square is not a mine
+                board[x][y] = '?';
                 continue;
             }
-            if (r < 12) {
-                board[x][y] = '*';
-            } else {
-                board[x][y] = '?';
-            }
+            int r = rand() % 100;
+            if (r < 12) board[x][y] = '*';
+            else board[x][y] = '?';
         }
-    for (int i = 0; i < 16; i++)
-        for (int j = 0; j < 16; j++) {
-            memory[i][j] = '?';
-        }
+    }
 }
 
 void visualize(int row, int col) {
+    if (row < 0 || row >= 16 || col < 0 || col >= 16 || memory[row][col] != ' ') 
+        return;
+
     int mine_count = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            int new_row = row + i;
-            int new_col = col + j;
-            if (new_row >= 0 && new_row < 16 && new_col >= 0 && new_col < 16) {
-                if (board[new_row][new_col] == '*') {
-                    mine_count++;
-                }
-            }
+            if (i == 0 && j == 0) continue;
+            int nr = row + i, nc = col + j;
+            if (nr >= 0 && nr < 16 && nc >= 0 && nc < 16 && board[nr][nc] == '*')
+                mine_count++;
         }
     }
-    board[row][col] = mine_count + '0';
-    memory[row][col] = board[row][col];
+
+    memory[row][col] = mine_count + '0';
+    
     if (mine_count == 0) {
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                int new_row = row + i;
-                int new_col = col + j;
-                if (new_row >= 0 && new_row < 16 && new_col >= 0 && new_col < 16) {
-                    if (memory[new_row][new_col] == '?') {
-                        visualize(new_row, new_col);
-                    }
-                }
+                visualize(row + i, col + j);
             }
         }
     }
 }
 
 int moves(int row, int col) {
-    switch (board[row][col])
-    {
-    case '*':
-        printf("Game Over!\n");
+    if (row < 0 || row >= 16 || col < 0 || col >= 16) return 0;
+    if (board[row][col] == '*') {
+        printf("Game Over! Hit a mine at %c%d\n", col + 'A', row + 1);
         return 1;
-    case '?':
-        memory[row][col] = board[row][col];
-        visualize(row, col);
-        break;
-    default:
-        memory[row][col] = board[row][col];
-        break;
     }
+    visualize(row, col);
     return 0;
 }
 
 int main() {
+    char square[10];
+    int row, col;
+
+    srand(time(NULL));
     initialize_board();
     print_board();
-    printf("Select a square to reveal: ");
-    char square[4];
-    scanf("%s", square);
-    int row = square[1] - '1';
-    int col = square[0] - 'A';
+
+    printf("Enter starting square: ");
+    scanf("%9s", square);
+    col = toupper(square[0]) - 'A';
+    row = atoi(&square[1]) - 1;
+
     create_mine(row, col);
-    print_board();
-    int end = 1;
+    moves(row, col);
+
+    int end = 0;
     while (end == 0) {
         system("cls");
         print_board();
-        printf("Select a square to reveal: ");
-        scanf("%s", square);
-        row = square[1] - '1';
-        col = square[0] - 'A';
+        printf("Select a square: ");
+        scanf("%9s", square);
+        col = toupper(square[0]) - 'A';
+        row = atoi(&square[1]) - 1;
         end = moves(row, col);
     }
+
     return 0;
 }
